7b983b9a81fbc071cacd9aeef0f3191ae6b43abc
Okoli Evans <mr.evans0075@gmail.com>
2022-10-30 11:26:48 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import profileImg from "../src/images/profile.jpg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="profile_img">
+          <img src={profileImg} alt='profile'></img>
+        </div>
       </header>
     </div>
   );
