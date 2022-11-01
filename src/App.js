25cb566f7ec6df6f555278d0061647d19a3f3b32
Okoli Evans <mr.evans0075@gmail.com>
2022-11-01 12:39:31 +0100

    final css styling, also edited some html titles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import profileImg from "../src/images/evans.png"
 import shareIcon from "../src/images/shareIcon2.png"
 import github from "../src/images/github_icon.png"
 import slack from "../src/images/slack.jpg"
+import zuriLogo from "../src/images/zuriLogo.webp"
 import './App.css';
 
 function App() {
@@ -30,7 +31,7 @@ function App() {
             <button id="books_python" title="get quality books on python coding"><a href="https://books.zuri.team/python-for-beginners?ref_id=mr.evans0075">Python Books</a></button>
           </div>
           <div id="row">
-            <button id="pitch" title="we work with only verified devs that passes our assessment"><a href="https://background.zuri.team">Background Check for Coders</a></button>
+            <button id="pitch" title="we work with only verified devs that pass our assessment"><a href="https://background.zuri.team">Background Check for Coders</a></button>
           </div>
           <div id="row">
             <button id="book_design" title="get quality designs for your books for free"><a href="https://books.zuri.team/design-rules">Design Books</a></button>
@@ -38,6 +39,13 @@ function App() {
           <div id="logos">
             <img  src={slack} alt="slack logo" id="slack_logo"></img>
             <img src={github} alt="slack logo" id="git_logo"></img>
+          </div>
+          <div id="footer">
+            <p id="zuri_text">Zuri<span id="fullstop">.</span>Internship</p>
+
+            <p id="HNG_text">HNG Internship 9 Frontend Task</p>
+
+            <img src={zuriLogo} alt="zuri logo" id="zuri_logo"></img>
           </div>
         </section>
       </header>
